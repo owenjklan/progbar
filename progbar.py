24439caf7718095fe52091c53a0637b2cbfc21ef
Owen Klan <owen.j.klan@gmail.com>
2019-12-03 20:51:29 +1000

    Improve documentation.
    Added better docstring for the TextProgressBar class so that
    the REPL's help() built-in function can be of some value.

--- progbar.py
+++ progbar.py
@@ -8,20 +8,27 @@ import random
 class TextProgressBar:
     """
     Text-mode progress bar supporting labels, colour and numeric output.
+
+    label: will be truncated to 16 characters by default. Shown at left
+    width: how many characters wide the progress bar will be
+    start: beginning value for the progress bar
+    end:   ending value for the progress bar when at "100%"
+    style: One of:
+        SYTLE_HASHES:      |###====|
+        SYTLE_BOXES1:      |▉▉▉▉▉░░|
+        STYLE_UNDERSCORED: |▉▉▉____|
+        STYLE_BOXES2:   -- Not Currently Implemented -- (Dec. 2019)
+    show_percent:   False:       20 of 100
+                    True:           20%
+    suffix:  Text to append to numeric/percentage readout.
+             Example:  suffix="bytes"  ==>    1023 of 2048 bytes
+             Note: Suffix is NOT displayed when 'show_percent' is True
     """
 
     STYLE_HASHES = 1
     STYLE_BOXES1 = 2
     STYLE_BOXES2 = 3
-
-    # def __init__(self, label, width, start, end):
-    #     self.label = label
-    #     self.start = start
-    #     self.end = end
-    #     self.width = width
-    #     self.scale = (width / end)
-    #     self.value = start
-    #     self.complete = False
+    STYLE_UNDERSCORED = 4
 
     def __init__(self, label, width, start, end, style=STYLE_HASHES,
                  show_percent=False, suffix=None):
@@ -44,6 +51,8 @@ class TextProgressBar:
             return '='
         elif style == cls.STYLE_BOXES1:
             return u"\u2591"
+        elif style == cls.STYLE_UNDERSCORED:
+            return "_"
         else:
             return '='
 
@@ -51,6 +60,8 @@ class TextProgressBar:
         if style == cls.STYLE_HASHES:
             return '#'
         elif style == cls.STYLE_BOXES1:
+            return u"\u2589"
+        elif style == cls.STYLE_UNDERSCORED:
             return u"\u2589"
         else:
             return '#'
@@ -89,12 +100,40 @@ class TextProgressBar:
             print
 
     def add(self, value):
+        """
+        Add the supplied value to the progress bar's current value.
+        Includes a cap to ensure that a value that is larger than
+        the end value cannot be set.
+        """
         self.value = self.value + value
         if self.value > self.end:
             self.value = self.end
 
 
+#
+# Test program to demonstrate the progress bar in action
+#
+# Usage:
+#  python progbar.py [style [percent]]
+#
+#  style:  1-4
+#  percent: "percent" to display percentage. Anything else to use X of Y
+#  suffix: optional suffix
+#
+#  
+#
+
 if __name__ == "__main__":
+    percent = False
+    style = 1
+    suffix = None
+    if len(sys.argv) > 1:
+        style = int(sys.argv[1])
+    if len(sys.argv) > 2:
+        percent = True if sys.argv[2].lower() == "percent" else False
+    if len(sys.argv) > 3:
+        suffix = sys.argv[3]
+
     random.seed(0xcafedead)
     sourcefiles = [("bag.txt", 39), ("readme.hlp", 123), ("main.cs", 321),
                    ("longassedfilenamasdfasdfasdfasdfe", 100),
@@ -107,8 +146,8 @@ if __name__ == "__main__":
     for srcfile in sourcefiles:
         tbr = TextProgressBar(
             srcfile[0], 80, 0, srcfile[1],
-            style=TextProgressBar.STYLE_BOXES1,
-            show_percent=False, suffix="bytes")
+            style=style,
+            show_percent=percent, suffix=suffix)
         for temp in range(0, srcfile[1]):
             tbr.add(random.randint(1, 50))
             tbr.render()
